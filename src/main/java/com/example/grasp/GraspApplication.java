21e9fb68bede1b3ec917925d96ec2c33b4b9d953
vkptimon <venkatakoushikp2002@gmail.com>
2026-02-18 00:25:15 +0530

    Update .gitattributes, .gitignore, maven-wrapper.properties, and 6 more files

---
+++ src/main/java/com/example/grasp/GraspApplication.java
@@ -0,0 +1,13 @@
+package com.example.grasp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraspApplication.class, args);
+	}
+
+}
